abb813ac4fdde51f43f1020896fba9b535bed34f
FZ <iamfayez@gmail.com>
2016-05-21 21:18:50 +0300

    some new misc tests

--- tesserocr_experiment.pyx
+++ tesserocr_experiment.pyx
@@ -15,23 +15,30 @@ cdef object _mode_to_bpp = {'1':1, 'L':8, 'P':8, 'RGB':24, 'RGBA':32, 'CMYK':32,
 
 cdef void _image_buffer2(image, cuchar_t **buff, int *width, int *height,
                          int *bpp, int *bpl):
+    """Read image meta data from unsafe pointers."""
     cdef uintptr_t buff_ptr
     # get buffer from unsafe pointers without copying it
     image.load()
-    ptrs = image.im.unsafe_ptrs
-    for f in ptrs:
-        name = f[0]
-        if name == 'xsize':  # width
-            width[0] = f[1]
-        elif name == 'ysize':  # height
-            height[0] = f[1]
-        elif name == 'image':  # buffer address
-            buff_ptr = f[1]
-            buff[0] = (<cuchar_t **>buff_ptr)[0]
-        elif name == 'pixelsize':  # bytes_per_pixel
-            bpp[0] = f[1]
-        elif name == 'linesize':  # bytes_per_line
-            bpl[0] = f[1]
+    ptrs = dict(image.im.unsafe_ptrs)
+    width[0] = ptrs['xsize']
+    height[0] = ptrs['ysize']
+    buff_ptr = ptrs['image']
+    buff[0] = (<cuchar_t **>buff_ptr)[0]
+    bpp[0] = ptrs['pixelsize']
+    bpl[0] = ptrs['linesize']
+    # for f in ptrs:
+    #     name = f[0]
+    #     if name == 'xsize':  # width
+    #         width[0] = f[1]
+    #     elif name == 'ysize':  # height
+    #         height[0] = f[1]
+    #     elif name == 'image':  # buffer address
+    #         buff_ptr = f[1]
+    #         buff[0] = (<cuchar_t **>buff_ptr)[0]
+    #     elif name == 'pixelsize':  # bytes_per_pixel
+    #         bpp[0] = f[1]
+    #     elif name == 'linesize':  # bytes_per_line
+    #         bpl[0] = f[1]
 
 
 cdef char *_image_to_text2(const unsigned char *buff, int width, int height, int bpp, int bpl,
@@ -77,6 +84,9 @@ def image_to_text2(image, const char *lang=_DEFAULT_LANG, const PageSegMode page
         char *text
 
     _image_buffer2(image, &buff, &width, &height, &bpp, &bpl)
+    # print width, height
+    # print bpp
+    # print bpl
 
     with nogil:
         text = _image_to_text2(buff, width, height, bpp, bpl,
@@ -84,4 +94,69 @@ def image_to_text2(image, const char *lang=_DEFAULT_LANG, const PageSegMode page
         if text == NULL:
             with gil:
                 raise RuntimeError('Failed to recognize image text.')
-    return _strip_and_free(text)
+    return _free_str(text)
+
+
+cdef Pix *raw_to_pix(cuchar_t *buff, int bpp, int width, int height, int bpl) nogil:
+    """Convert PIL image to Pix.
+
+    Applies the same logic done by tesseract's api.SetImage."""
+    cdef:
+        int x
+        int y
+        int wpl
+        uint *data
+        Pix *pix
+    bpp = bpp * 8
+    pix = pixCreate(width, height, 32 if bpp == 24 else bpp)
+    wpl = pixGetWpl(pix)
+    data = pixGetData(pix)
+    if bpp == 8:
+        # Greyscale just copies the bytes in the right order.
+        for y in xrange(height):
+            for x in xrange(width):
+                SET_DATA_BYTE(data, x, buff[x])
+            data += wpl
+            buff += bpl
+    elif bpp == 24:
+        # Put the colors in the correct places in the line buffer.
+        for y in xrange(height):
+            for x in xrange(width):
+                SET_DATA_BYTE(data, COLOR_RED, buff[3 * x])
+                SET_DATA_BYTE(data, COLOR_GREEN, buff[3 * x + 1])
+                SET_DATA_BYTE(data, COLOR_BLUE, buff[3 * x + 2])
+                data += 1
+            buff += bpl
+    elif bpp == 32:
+        # Maintain byte order consistency across different endianness.
+        for y in xrange(height):
+            for x in xrange(width):
+                data[x] = (buff[x * 4] << 24) | (buff[x * 4 + 1] << 16) | (buff[x * 4 + 2] << 8) | buff[x * 4 + 3]
+            data += wpl
+            buff += bpl
+    else:
+        with gil:
+            raise RuntimeError("Cannot convert RAW image to Pix with bpp = {}".format(bpp))
+    return pix
+
+def image_to_text3(image, const char *lang=_DEFAULT_LANG, const PageSegMode psm=PSM_AUTO,
+                   const char *path=_DEFAULT_PATH):
+    cdef:
+        Pix *pix
+        cuchar_t *buff = NULL
+        int width = 0
+        int height = 0
+        int bpp = 0
+        int bpl = 0
+        char *text
+
+    _image_buffer2(image, &buff, &width, &height, &bpp, &bpl)
+
+    with nogil:
+        pix = raw_to_pix(buff, bpp, width, height, bpl)
+        text = _image_to_text(pix, lang, psm, path)
+        if text == NULL:
+            with gil:
+                raise RuntimeError('Failed recognize picture')
+
+    return _free_str(text)
