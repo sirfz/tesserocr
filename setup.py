4efe0ac579555d204781441d0385c037094b1572
sirfz <iamfayez@gmail.com>
2018-08-13 19:20:15 +0300

    Python 3.7 support

--- setup.py
+++ setup.py
@@ -178,6 +178,7 @@ setup(name='tesserocr',
           'Programming Language :: Python :: 3.4',
           'Programming Language :: Python :: 3.5',
           'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7',
           'Programming Language :: Python :: Implementation :: CPython',
           'Programming Language :: Python :: Implementation :: PyPy',
           'Programming Language :: Cython'
