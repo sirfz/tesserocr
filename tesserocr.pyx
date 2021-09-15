bf4c2a5b7810a76cc222bcc1c507b6b5e86b513d
zdenop <zdenop@gmail.com>
2021-09-15 14:23:28 +0200

    fix ProcessPage()/ fixes #271

--- tesserocr.pyx
+++ tesserocr.pyx
@@ -2190,7 +2190,7 @@ cdef class PyTessBaseAPI:
         raise RuntimeError('No renderers enabled')
 
     def ProcessPage(self, outputbase, image, int page_index, filename,
-                    retry_config=None, int timeout=0):
+                    title="", retry_config=None, int timeout=0):
         """Turn a single image into symbolic text.
 
         See :meth:`ProcessPages` for descriptions of the keyword arguments
@@ -2227,6 +2227,7 @@ cdef class PyTessBaseAPI:
         if pix == NULL:
             raise RuntimeError('Failed to read image')
         if renderer != NULL:
+            renderer.BeginDocument(title)
             if retry_config is not None:
                 py_config = _b(retry_config)
                 cconfig = py_config
@@ -2235,6 +2236,7 @@ cdef class PyTessBaseAPI:
             try:
                 return self._baseapi.ProcessPage(pix, page_index, cfname, cconfig, timeout, renderer)
             finally:
+                renderer.EndDocument()
                 pixDestroy(&pix)
                 del renderer
         raise RuntimeError('No renderers enabled')
