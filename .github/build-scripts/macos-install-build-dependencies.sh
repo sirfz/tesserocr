261e2235b0ada3b2adbf333274b599d2c6de2621
sirfz <iamfayez@gmail.com>
2024-03-07 14:26:49 +0200

    github actions: build with jpeg/webp/tiff support

--- .github/build-scripts/macos-install-build-dependencies.sh
+++ .github/build-scripts/macos-install-build-dependencies.sh
@@ -2,4 +2,4 @@
 set -ex
 
 # leptonica dependencies
-brew install libpng zlib
+brew install libpng libjpeg libtiff libwebp zlib
--- .github/build-scripts/musllinux-install-build-dependencies.sh
+++ .github/build-scripts/musllinux-install-build-dependencies.sh
@@ -2,4 +2,4 @@
 set -ex
 
 # leptonica dependencies
-apk add libpng-dev zlib-dev
+apk add libpng-dev libjpeg-dev libtiff-dev libwebp-dev zlib-dev
